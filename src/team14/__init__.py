82cb8ffa9ceafb29c1ebc23b968a74f271ed7d96
Ryan Thomson <robosaurus@gmail.com>
2025-03-10 15:09:13 -0700

    added NiftyConfusionMatrix class, to display a nice confusion matrix

--- src/team14/__init__.py
+++ src/team14/__init__.py
@@ -2,3 +2,4 @@ __all__ = ["AudioFile", "SpeechCommandsDataset"]
 
 from .speech_commands_dataset import SpeechCommandsDataset
 from .audio_file import AudioFile
+from .nifty_confusion_matrix import NiftyConfusionMatrix
